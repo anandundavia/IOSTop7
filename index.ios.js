39391e694f0467bd8c6b78b8d02dc2b38132b415
anand <anand.u@innovify.in>
2017-06-16 16:06:48 +0530

    This configuration works with Android

--- index.ios.js
+++ index.ios.js
@@ -1,23 +1,26 @@
 import React, {Component} from "react";
-import {AppRegistry} from "react-native";
+import {AppRegistry, BackHandler} from "react-native";
 import {StackNavigator} from "react-navigation";
-
+import Consts from "./app/consts/Consts";
 import Main from "./app/components/Main";
 
 export default class IOSTop7 extends Component {
+
     static navigationOptions = {
         title: 'Welcome',
         header: null
     };
 
     render() {
-        return <Main/>;
+        return <Main navigation={this.props.navigation}/>;
     }
+
 }
 
-
 const Top7 = StackNavigator({
-    Home: {screen: IOSTop7},
+    "Home": {screen: IOSTop7},
+    "Splash": {screen: Consts.SCREENS.SPLASH},
+    "Login": {screen: Consts.SCREENS.LOG_IN}
 });
 
 AppRegistry.registerComponent('IOSTop7', () => Top7);
